45f8d3ca8a5329e6f99d806b9042711b6e7df813
rocky <cxilymh@gamil.com>
2015-10-15 18:56:09 +0800

    添加task相关内容

--- task-schedule-client/src/main/java/com/link/schedule/client/annotation/TaskConfig.java
+++ task-schedule-client/src/main/java/com/link/schedule/client/annotation/TaskConfig.java
@@ -18,4 +18,80 @@ public class TaskConfig {
     private boolean isConcurrent;
 
     private String host;
+
+    private String methodName;
+
+    private String className;
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getApplication() {
+        return application;
+    }
+
+    public void setApplication(String application) {
+        this.application = application;
+    }
+
+    public String getCronExpress() {
+        return cronExpress;
+    }
+
+    public void setCronExpress(String cronExpress) {
+        this.cronExpress = cronExpress;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public boolean isConcurrent() {
+        return isConcurrent;
+    }
+
+    public void setIsConcurrent(boolean isConcurrent) {
+        this.isConcurrent = isConcurrent;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
 }
